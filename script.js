09240cd2eeca4d92b6fcdbb06dd335de2803a893
Allyson <allysoneduardo04@gmail.com>
2021-01-24 01:11:44 -0300

    feat: added 'actions' array with functions to add points, clear last character and do expression

--- script.js
+++ script.js
@@ -45,3 +45,35 @@ operation = {
         document.getElementById("calculator-screen").value += "/"
     }
 }
+
+action = {
+    clear() {
+
+        //defining a var with value from input
+        var rawExpression = document.getElementById("calculator-screen").value
+
+        //removing ONLY the last character added
+        var cutExpression = rawExpression.slice(0, -1)
+
+        //reseting string on input
+        document.getElementById("calculator-screen").value = ""
+        document.getElementById("calculator-screen").value = cutExpression
+    },
+    addPoint(){
+
+        //adding point for float numbers
+        document.getElementById("calculator-screen").value += "."
+    },
+    equals(){
+        //first we transform "x" to "*", to prevent errors in application
+        var rawExpression = document.getElementById("calculator-screen").value
+        let properExpression = rawExpression.replace("x", "*")
+
+
+        //then we use eval() to interpret var finalExpression as a javascript code
+        let finalExpression = eval(properExpression)
+        document.getElementById("calculator-screen").value = ""
+        document.getElementById("calculator-screen").value = finalExpression
+
+    }
+}
