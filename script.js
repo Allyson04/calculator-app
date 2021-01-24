c4b52089872eb9149f7950ee01c5ce7e568b3ac8
Allyson <allysoneduardo04@gmail.com>
2021-01-24 01:30:27 -0300

    refactor: replaced array of numbers functions to function receiving values

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
-addNumber = {
-    one() {
-        document.getElementById("calculator-screen").value += 1
-    },
-    two(){
-        document.getElementById("calculator-screen").value += 2
-    },
-    three(){
-        document.getElementById("calculator-screen").value += 3
-    },
-    four(){
-        document.getElementById("calculator-screen").value += 4
-    },
-    five(){
-        document.getElementById("calculator-screen").value += 5
-    },
-    six(){
-        document.getElementById("calculator-screen").value += 6
-    },
-    seven(){
-        document.getElementById("calculator-screen").value += 7
-    },
-    eight(){
-        document.getElementById("calculator-screen").value += 8
-    },
-    nine(){
-        document.getElementById("calculator-screen").value += 9
-    },
-    zero(){
-        document.getElementById("calculator-screen").value += 0
-    }
+function addNumber(numberClicked){
+    document.getElementById("calculator-screen").value += numberClicked
 }
 
 operation = {
