59da457cee1dba4f9fc7b62cecbc3e7c7f325e52
Allyson <allysoneduardo04@gmail.com>
2021-01-24 23:28:57 -0300

    refactor: creating a variable to always get the element 'calculator-screen'

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
+//In this document we are going to always call the element "calculator-screen"
+//so why not creating a var for him, instead of writing all the caller everytime?
+let getCalculator = document.getElementById("calculator-screen");
+
 function addNumber(numberClicked){
-    document.getElementById("calculator-screen").value += numberClicked
+    getCalculator.value += numberClicked
 }
 
 function addOperation(operationNeeded){
-    document.getElementById("calculator-screen").value += operationNeeded
+    getCalculator.value += operationNeeded
 }
 
 function eraseInput(){
-    document.getElementById("calculator-screen").value = ""
+    getCalculator.value = ""
 }
 
 action = {
     clear() {
 
         //defining a var with value from input
-        var rawExpression = document.getElementById("calculator-screen").value
+        var rawExpression = getCalculator.value
 
         //removing ONLY the last character added
         var cutExpression = rawExpression.slice(0, -1)
 
         //reseting string on input
         eraseInput()
-        document.getElementById("calculator-screen").value = cutExpression
+        getCalculator.value = cutExpression
     },
     addPoint(){
 
         //adding point for float numbers
-        document.getElementById("calculator-screen").value += "."
+        getCalculator.value += "."
     },
     equals(){
         //first we transform "x" to "*", to prevent errors in application
-        var rawExpression = document.getElementById("calculator-screen").value
+        let rawExpression = getCalculator.value
         let properExpression = rawExpression.replace("x", "*")
 
 
         //then we use eval() to interpret var finalExpression as a javascript code
         let finalExpression = eval(properExpression)
         eraseInput()
-        document.getElementById("calculator-screen").value = finalExpression
+        getCalculator.value = finalExpression
     }
 }
